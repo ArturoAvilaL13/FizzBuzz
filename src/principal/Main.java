32ce41cb9bb6124341903c9d2d79dc213b5045a7
ArturoAvilaL13 <arturo.avila1319@gmail.com>
2023-11-24 12:59:29 -0600

    Desplilega FizzBuzz para numeros multiplos de 3 y 5

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -8,7 +8,9 @@ public class Main {
 		
 		for (int i=1;i<=cantidadNumeros;i++) {
 			
-			if(i%3==0)
+			if((i%3==0)&&(i%5==0))
+				System.out.println("FizzBuzz");
+			else if(i%3==0)
 				System.out.println("Fizz");
 			else if(i%5==0)
 				System.out.println("Buzz");
