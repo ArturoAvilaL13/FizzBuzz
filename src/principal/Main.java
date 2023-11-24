d4e4017588b55004b74f4ff01f9e2ad88fc54097
ArturoAvilaL13 <arturo.avila1319@gmail.com>
2023-11-24 12:34:31 -0600

    Se creo el for para imprimir n numeros

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int cantidadNumeros = 100;
 		
+		for (int i=0;i<=cantidadNumeros;i++) {
+			System.out.println("N. "+i);
+		}
 	}
 
 }
